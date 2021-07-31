24e9d19b0ce01ce67f82d4b959f59042b6493d34
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-31 22:37:25 +0200

    Initial Commit

---
+++ Shared/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 31/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
